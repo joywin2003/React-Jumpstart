2fcae62cb8074625edcf4d88c5cf398874f2ff5e
joywin2003 <21a20.joywin@sjec.ac.in>
2023-08-05 13:54:52 +0530

    lifting up states

--- src/Components/NewPost.jsx
+++ src/Components/NewPost.jsx
@@ -1,20 +1,23 @@
 import classes from './NewPost.module.css';
 import { useState } from 'react';
-function NewPost() {
+function NewPost(props) {
     const [body, setBody] = useState('0/200');
-    function changeBodyHandler(event) {
+    function changeHandler(event) {
         setBody(event.target.value);
     }
     return (
         <form className={classes.form}>
             <p>
                 <label htmlFor="body">Text</label>
-                <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+                <textarea id="body" required rows={3} onChange={(event) => {
+                    changeHandler(event),
+                    props.onChangeBody(event)
+                }} />
             </p>
             <p>{body.length}/200</p>
             <p>
                 <label htmlFor="name">Your name</label>
-                <input type="text" id="name" required />
+                <input type="text" id="name" required onChange={props.onChangeAuthor} />
             </p>
         </form>
     );
--- src/Components/PostList.jsx
+++ src/Components/PostList.jsx
@@ -1,12 +1,23 @@
 import Post from "./Post"
 import classes from './PostList.module.css'
 import NewPost from "./NewPost";
+import { useState } from 'react';
+
 function PostList(){
+    const [enteredBody,setEnteredBody] = useState('');
+    const [enteredAuthor,setEnteredAuther] = useState('');
+
+    function changeBodyHandler(event) {
+        setEnteredBody(event.target.value);
+    }
+    function changeauthorHandler(event) {
+        setEnteredAuther(event.target.value);
+    }
     return(
         <>
-        <NewPost/>
+        <NewPost onChangeBody={changeBodyHandler} onChangeAuthor={changeauthorHandler}/>
         <ul className={classes.posts}>
-            <Post author="Joywin" body = "This is my post"/>
+            <Post author={enteredAuthor} body = {enteredBody}/>
             <Post author="Chris" body = "This is my post"/>
             <Post author="Canon" body = "This is my post"/>
             <Post author="Joswin" body = "This is my post"/>
