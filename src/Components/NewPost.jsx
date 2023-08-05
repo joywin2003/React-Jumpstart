21d42741941985e278cc4f81eadbb530d0356041
joywin2003 <21a20.joywin@sjec.ac.in>
2023-08-05 13:33:49 +0530

    added states

--- src/Components/NewPost.jsx
+++ src/Components/NewPost.jsx
@@ -1,22 +1,23 @@
 import classes from './NewPost.module.css';
-
+import { useState } from 'react';
 function NewPost() {
-  function changeBodyHandler(event) {
-    console.log(event.target.value);
-  }
-
-  return (
-    <form className={classes.form}>
-      <p>
-        <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
-      </p>
-      <p>
-        <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
-      </p>
-    </form>
-  );
+    const [body, setBody] = useState('0/200');
+    function changeBodyHandler(event) {
+        setBody(event.target.value);
+    }
+    return (
+        <form className={classes.form}>
+            <p>
+                <label htmlFor="body">Text</label>
+                <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+            </p>
+            <p>{body.length}/200</p>
+            <p>
+                <label htmlFor="name">Your name</label>
+                <input type="text" id="name" required />
+            </p>
+        </form>
+    );
 }
 
 export default NewPost;
