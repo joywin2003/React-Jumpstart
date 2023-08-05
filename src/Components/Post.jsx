011b9e268a43837a3190ca437c1ec8badd0f55fb
joywin2003 <21a20.joywin@sjec.ac.in>
2023-08-05 12:55:19 +0530

    css styles

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,10 +1,10 @@
-
+import classes from './Post.module.css';
 
 function Post(props){
     return(
-        <div>
-            <h1>My name is {props.author}</h1>
-            <p>{props.body}</p>
+        <div className={classes.post}>
+            <h1 className={classes.author}>My name is {props.author}</h1>
+            <p className={classes.text}>{props.body}</p>
         </div>
     )
 }
