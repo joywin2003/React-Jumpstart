656e8a6f828b5d8f1823d9ad747650f13175130a
joywin2003 <21a20.joywin@sjec.ac.in>
2023-08-09 22:56:23 +0530

    clean up function

--- sideEffects/src/components/Login/Login.js
+++ sideEffects/src/components/Login/Login.js
@@ -12,10 +12,18 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
-  },[enteredEmail,enteredPassword]);
+    const identifier = setTimeout(()=>{
+      console.log("inside availability");
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    },1000);
+    return () =>{
+      console.log("CLEAN UP");
+      clearTimeout(identifier);
+    }
+    },[enteredEmail,enteredPassword])
+    
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
