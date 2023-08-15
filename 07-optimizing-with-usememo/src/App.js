4c7bac0d7a0eb48e7bd3bf2e79896ba8faabde98
joywin2003 <21a20.joywin@sjec.ac.in>
2023-08-15 22:03:22 +0530

    removed gitignore

---
+++ 07-optimizing-with-usememo/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState, useCallback, useMemo } from 'react';
+
+import './App.css';
+import DemoList from './components/Demo/DemoList';
+import Button from './components/UI/Button/Button';
+
+function App() {
+  const [listTitle, setListTitle] = useState('My List');
+
+  const changeTitleHandler = useCallback(() => {
+    setListTitle('New Title');
+  }, []);
+  const new_items = useMemo(()=> [5, 3, 1, 10, 9],[])
+  return (
+    <div className='app'>
+      <DemoList title={listTitle} items={new_items} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ 07-optimizing-with-usememo/src/components/Demo/DemoList.js
@@ -0,0 +1,25 @@
+import { useMemo } from "react";
+import React from "react";
+
+import classes from "./DemoList.module.css";
+
+const DemoList = (props) => {
+  const { items } = props;
+
+  const sortedList = useMemo(() => {
+    console.log("In demo list");
+    return items.sort((a, b) => a - b);
+  }, [items]);
+  return (
+    <div className={classes.list}>
+      <h2>{props.title}</h2>
+      <ul>
+        {sortedList.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default React.memo(DemoList);
