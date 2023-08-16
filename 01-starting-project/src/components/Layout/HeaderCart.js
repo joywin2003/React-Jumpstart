5398a390307281bc83835823f9c16b9936cd4783
joywin2003 <21a20.joywin@sjec.ac.in>
2023-08-16 17:59:17 +0530

    added consumer context to HeaderCart

--- 01-starting-project/src/components/Layout/HeaderCart.js
+++ 01-starting-project/src/components/Layout/HeaderCart.js
@@ -1,13 +1,19 @@
 import CartIcon from "../Cart/CartIcon"
 import classes from "./HeaderCart.module.css"
+import CartContext from "../../store/cart-context";
+import { useContext } from "react";
 
 const HeaderCart = (props) => {
+    const value = useContext(CartContext)
+    const numberOfCartItems = value.items.reduce((curNumber, item) => {
+        return curNumber+item.amount;
+    },0)
     return (
         <>
             <button className={classes.button} onClick={props.showCartHandler}>
                 <span className={classes.icon}><CartIcon /></span>
                 <span>Your Cart</span>
-                <span className={classes.badge}>3</span>
+                <span className={classes.badge}>{numberOfCartItems}</span>
             </button>
         </>
     )
--- 01-starting-project/src/store/cart-context.js
+++ 01-starting-project/src/store/cart-context.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const CartContext  =React.createContext({
     items:[],
     totalamount:0,
